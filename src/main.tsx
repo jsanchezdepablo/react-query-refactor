661551dc600c2bdd897e1a9da04a73671f8e5348
JESUS SANCHEZ DE PABLO PEREZ <jesanchp@emeal.nttdata.com>
2023-08-11 21:04:38 +0200

    chore: Add new library tanstack query and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
